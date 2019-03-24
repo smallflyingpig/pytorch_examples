d0b8eb3f1fc6cc4b40b0814a3cd7ace080bcc2df
lijiguo <lijiguo@bytedance.com>
2019-03-24 18:37:06 +0800

    fix a bug

--- ChannelPool/main.py
+++ ChannelPool/main.py
@@ -126,12 +126,7 @@ def main(args):
     # Training
     trainer = Trainer(net, trainloader, testloader, optimizer, args.device, criterion, args.logger, args.writer, args.model_dir_full)
     trainer.train(args.epoch, val_interval=args.val_interval, lr_scheduler=scheduler, start_epoch=start_epoch, best_acc=best_acc)
-    # test_dict = trainer.test()
-    # for epoch in range(start_epoch, start_epoch+args.epoch):
-    #     scheduler.step()
-    #     train(epoch)
-    #     acc = test(epoch)
-    #     update_best_acc(acc, best_acc)
+    test_dict = trainer.test()
     
     args.logger.info("training and test end, best:{}, mean:{}, std:{}%".format(test_dict['best'] * 100., test_dict['mean']*100., test_dict['std']))
 
