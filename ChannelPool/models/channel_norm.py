025262ff3d5c3e08f9f0d08072fd3143b8fb8cc6
lijiguo <lijiguo@bytedance.com>
2019-03-26 12:19:07 +0800

    add affine param for channel norm

--- ChannelPool/models/channel_norm.py
+++ ChannelPool/models/channel_norm.py
@@ -1,14 +1,16 @@
 import torch.nn as nn
 import torch.nn.functional as F 
 class ChannelNorm(nn.Module):
-    def __init__(self):
+    def __init__(self, affine=False):
         super(ChannelNorm, self).__init__()
+        self.affine = affine
+        self.instance_norm = nn.InstanceNorm1d(affine=affine)
     
     def forward(self, x):
         B, C, H, W = x.size()
         x = x.permute(0, 2,3, 1).view(B, H*W, C)
         
-        x = F.instance_norm(x)
+        x = self.instance_norm(x)
         x = x.view(B, H, W, C).permute(0,3,1,2)
 
         return x
