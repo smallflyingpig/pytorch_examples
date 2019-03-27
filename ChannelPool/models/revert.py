6dd263a892bbe86e9d34907a307075ca847295dc
lijiguo <lijiguo@bytedance.com>
2019-03-27 13:44:48 +0800

    use abs as revert

--- ChannelPool/models/revert.py
+++ ChannelPool/models/revert.py
@@ -5,13 +5,13 @@ import torch.nn.functional as F
 class Revert(nn.Module):
     def __init__(self, input_channel):
         super(Revert, self).__init__()
-        self.bn = nn.BatchNorm2d(input_channel*2)
-        self.conv = nn.Conv2d(input_channel*2, input_channel, kernel_size=1, groups=input_channel, bias=False)
+        # self.bn = nn.BatchNorm2d(input_channel*2)
+        # self.conv = nn.Conv2d(input_channel*2, input_channel, kernel_size=1, groups=input_channel, bias=False)
 
     def forward(self, x):
-        B,C,H,W = x.size()
-        x = x.view(B,C,1,H,W)
-        x = torch.cat([x, -x], dim=2).view(B,C*2, H, W)
-        x = self.bn(F.relu(x))
-        x = self.conv(x)
-        return x
+        # B,C,H,W = x.size()
+        # x = x.view(B,C,1,H,W)
+        # x = torch.cat([x, -x], dim=2).view(B,C*2, H, W)
+        # x = self.bn(F.relu(x))
+        # x = self.conv(x)
+        return torch.abs(x)
